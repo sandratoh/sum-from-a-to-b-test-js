886354442a75553d6c1da369386cab749653e360
Sandra To <sandra.to.e@gmail.com>
2021-02-16 04:33:36 +0000

    Recursion function works with logic and pattern underneath

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 
 const sum = (fromN, toN) => {
   // Sum all the values from fromN up to toN
-  let sum = 0;
-  // recursion case: if number > fromN
 
-  // base case: if number is fromN
-  sum += fromN;
-  return sum;
+  // base case
+  if (toN === fromN) {
+    return fromN;
+  }
+  // recursion case: if number > fromN
+  return toN + sum(fromN, toN - 1);
 };
 
-// };
 
 module.exports = sum;
 
-// sum (0) = 0
-// sum (0, 1) = 1 + 0
-// sum (0, 2) = 2 + 1 + 0
-// sum (0, 3) = 3 + 2 + 1 + 0
-// sum (0, 4) = 4 + 3 + 2 + 1 + 0
+// Logic:
+// sum(0, 0) = 0
+// sum(0, 1) = 1 + 0
+// sum(0, 2) = 2 + 1 + 0
+// sum(0, 3) = 3 + 2 + 1 + 0
+// sum(0, 4) = 4 + 3 + 2 + 1 + 0
+
+// Pattern:
+// sum(0, 0) = 0
+// sum(0, 1) = 1 + sum(0, 0)
+// sum(0, 2) = 2 + sum(0, 1)
+// sum(0, 3) = 3 + sum(0, 2)
+// sum(0, 4) = 4 + sum(0, 3)
+
+// RECURSION IMPLEMENTATION
+// sum(fromN, toN) = toN + sum(fromN, toN - 1);
