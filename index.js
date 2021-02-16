46b7c7484142bd0dd57d82077de1a600dbb5dbb6
Sandra To <sandra.to.e@gmail.com>
2021-02-16 04:15:25 +0000

    base case added

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 
-function sum(fromN, toN) {
+const sum = (fromN, toN) => {
   // Sum all the values from fromN up to toN
-}
+  let sum = 0;
+  // recursion case: if number > fromN
+
+  // base case: if number is fromN
+  sum += fromN;
+  return sum;
+};
+
+// };
 
 module.exports = sum;
+
+// sum (0) = 0
+// sum (0, 1) = 1 + 0
+// sum (0, 2) = 2 + 1 + 0
+// sum (0, 3) = 3 + 2 + 1 + 0
+// sum (0, 4) = 4 + 3 + 2 + 1 + 0
